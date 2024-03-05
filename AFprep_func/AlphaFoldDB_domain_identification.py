ddfe3e7850c8cb3d4c4d2764bb0a94238b27badf
Ingelise Holland-Kaye <160491521+IngeliseH@users.noreply.github.com>
2024-03-05 16:38:20 +0000

    removed need for dictionary relating uniprot ids to AlphaFold ids

--- AFprep_func/AlphaFoldDB_domain_identification.py
+++ AFprep_func/AlphaFoldDB_domain_identification.py
@@ -1,17 +1,47 @@
 #identifying domains from AlphaFold DB
-
-#reading in list of accession codes in AlphaFold DB - should use to give exception if AlphaFoldDB does not have prediction for input id
-curl http://ftp.ebi.ac.uk/pub/databases/alphafold/accession_ids.txt -o accession_ids.txt
+#importing required packages
+import os
 import requests
-url = "http://ftp.ebi.ac.uk/pub/databases/alphafold/accession_ids.txt"
-response = requests.get(url)
-print(response)
 
-accession_contents = open('accession_ids.txt', 'r').readlines()
-accession_dict = {}
-for i in accession_contents:
-    accession_dict[i.split(',')[0]] = i.split(',')[3]
-accession_id = 'P14618'
-alphafold_id = None
-if accession_id in accession_dict:
-    alphafold_id = accession_dict[accession_id]
+def read_AFDB_json(accession_id, database_version="v4"):
+    """
+    Fetches and returns the JSON content from the AlphaFold Database (AFDB) for a given accession ID.
+
+    This function queries the AFDB for the predicted aligned error file associated with a specific accession ID. It handles errors where a file associated to the accession ID cannot be retrieved from the AFDB.
+
+    Parameters:
+    - accession_id (str): The accession ID for which to fetch the corresponding AlphaFold predicted aligned error JSON.
+    - database_version (str, optional): The version of the database to query. Default is "v4".
+
+    Returns:
+    - dict or None: The JSON content as a dictionary if the request is successful; otherwise, None.
+
+    Prints an error message and returns None if:
+    - The accession ID is not found in the predefined dictionary.
+    - The HTTP request to retrieve the file fails (e.g., file not found, network problems).
+
+    Note:
+    - This function requires the `requests` library to make HTTP requests.
+    - The function assumes that alphafold ids take the form AF-[a UniProt accession]-F1. If there are multiple fragments associated with a uniprot id this will only take fragment 1
+    """
+    alphafold_id = f'AF-{accession_id}-F1'
+    
+    json_url = f'https://alphafold.ebi.ac.uk/files/{alphafold_id}-predicted_aligned_error_{database_version}.json'
+    
+    try:
+        response = requests.get(json_url)
+        response.raise_for_status()  # Raises an HTTPError if the status is 4xx, 5xx
+    except requests.exceptions.HTTPError as e:
+        # Specific handling for HTTP errors (e.g., file not found on the server)
+        print(f"HTTP Error: Could not retrieve file from {json_url}. Error message: {e}")
+        return None
+    except requests.exceptions.RequestException as e:
+        # Broad exception for other issues, like network problems
+        print(f"Error: A problem occurred when trying to retrieve {json_url}. Error message: {e}")
+        return None
+    
+    # If everything went smoothly, return the content of the file
+    return response.json()
+
+#testing using Ana2
+read_AFDB_json("Q9XZ31")
