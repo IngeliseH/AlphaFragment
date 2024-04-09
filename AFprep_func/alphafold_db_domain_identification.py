1cbe8044e54f4bc59a8c833519b2dc8a041478eb
Ingelise Holland-Kaye <160491521+IngeliseH@users.noreply.github.com>
2024-04-09 14:28:00 +0100

    changed indents in docstrings in alphafold_db_domain_identification as test for documentation

--- AFprep_func/alphafold_db_domain_identification.py
+++ AFprep_func/alphafold_db_domain_identification.py
@@ -4,19 +4,14 @@ Predicted Aligned Error (PAE) data for identifying protein domains. Domains are
 determined from PAE values, grouping residues with similar PAE thresholds.
 
 Functions:
-
-- read_afdb_json(accession_id, database_version="v4"): Retrieves PAE data.
-
-- find_domain_by_res(domains, res): Helper function to identify the domain a
-  residue is in.
-
-- find_domains_from_pae(pae): Groups residues into domains based on PAE.
+  - read_afdb_json(accession_id, database_version="v4"): Retrieves PAE data.
+  - find_domain_by_res(domains, res): Helper function to identify the domain a
+    residue is in.
+  - find_domains_from_pae(pae): Groups residues into domains based on PAE.
 
 Dependencies:
-
-- requests: Used for making HTTP requests to the AlphaFold Database.
-
-- AFprep_func.classes.Domain: The Domain class used to represent protein domains.
+  - requests: Used for making HTTP requests to the AlphaFold Database.
+  - AFprep_func.classes.Domain: The Domain class used to represent protein domains.
 """
 
 #importing required packages
@@ -28,40 +23,29 @@ def read_afdb_json(accession_id, database_version="v4"):
     Fetches and returns the Predicted Aligned Error (PAE) data from the
     AlphaFold Database (AFDB) for a given UniProt accession ID.
 
-    This function queries the AFDB for the PAE associated with a specific
-    accession ID and the chosen database version. It directly extracts and
-    returns the 'predicted_aligned_error' data from the JSON response.
-
     Parameters:
-
-    - accession_id (str): The accession ID for which to fetch the corresponding
-      AlphaFold PAE data.
-
-    - database_version (str, optional): The version of the database to query.
-      Default is "v4".
+      - accession_id (str): The accession ID for which to fetch the corresponding
+        AlphaFold PAE data.
+      - database_version (str, optional): The version of the database to query.
+        Default is "v4".
 
     Returns:
-
-    - list of lists or None: The predicted_aligned_error data as a list of
-      lists if the request is successful and the data is present; otherwise,
-      None.
+      - list of lists or None: The predicted_aligned_error data as a list of
+        lists if the request is successful and the data is present; otherwise,
+        None.
 
     Prints an error message and returns None if:
-
-    - The HTTP request to retrieve the file fails (e.g., file not found, network
-      problems).
-
-    - The 'predicted_aligned_error' data cannot be found within the response,
-      indicating either an issue with the response data structure or the absence
-      of PAE data for the provided accession ID.
+      - The HTTP request to retrieve the file fails (e.g., file not found, network
+        problems).
+      - The 'predicted_aligned_error' data cannot be found within the response,
+        indicating either an issue with the response data structure or the absence
+        of PAE data for the provided accession ID.
 
     Note:
-
-    - This function requires the `requests` library to make HTTP requests.
-
-    - The function assumes that alphafold ids take the form
-      'AF-[a UniProt accession]-F1.' If there are multiple fragments associated
-      with a uniprot id this will only take fragment 1
+      - This function requires the `requests` library to make HTTP requests.
+      - The function assumes that alphafold ids take the form
+        'AF-[a UniProt accession]-F1.' If there are multiple fragments associated
+        with a uniprot id this will only take fragment 1
     """
     alphafold_id = f'AF-{accession_id}-F1'
     json_url = (
@@ -99,14 +83,11 @@ def find_domain_by_res(domains, res):
     Helper function to find the domain that contains a given residue.
 
     Parameters:
-
-    - domains (list of Domain): The list of current domain objects.
-
-    - res (int): The residue index to find within the domains.
+      - domains (list of Domain): The list of current domain objects.
+      - res (int): The residue index to find within the domains.
 
     Returns:
-
-    - Domain object if the residue is found within a domain, None otherwise.
+      - Domain object if the residue is found within a domain, None otherwise.
     """
     for domain in domains:
         if domain.start <= res <= domain.end:
@@ -122,51 +103,42 @@ def find_domains_from_pae(pae, res_dist_cutoff = 10, close_pae_val = 4, further_
     residues.
 
     Parameters:
-
-    - pae (list of lists): A 2D matrix of PAE values between residue pairs,
-      where pae[i][j] is the PAE between residues i and j.
-    
-    - res_dist_cutoff (int, optional): The distance threshold for residues to be
-      evaluated using the close_pae_val threshold. Default is 10. Should not be
-      below 4 as residues closer than this are ignored, because there is always
-      high confidence in relative position of residues this close.
-
-    - close_pae_val (int, optional): The PAE threshold which residue pairs must
-      surpass to be considered within the same domain, if the distance between
-      them is between 4 and res_dist_cutoff. Default is 4.
-    
-    - further_pae_val (int, optional): The PAE threshold which residue pairs must
-      surpass to be considered within the same domain, if the distance between
-      them is greater than res_dist_cutoff. Default is 11.
+      - pae (list of lists): A 2D matrix of PAE values between residue pairs,
+        where pae[i][j] is the PAE between residues i and j.
+      - res_dist_cutoff (int, optional): The distance threshold for residues to be
+        evaluated using the close_pae_val threshold. Default is 10. Should not be
+        below 4 as residues closer than this are ignored, because there is always
+        high confidence in relative position of residues this close.
+      - close_pae_val (int, optional): The PAE threshold which residue pairs must
+        surpass to be considered within the same domain, if the distance between
+        them is between 4 and res_dist_cutoff. Default is 4.
+      - further_pae_val (int, optional): The PAE threshold which residue pairs must
+        surpass to be considered within the same domain, if the distance between
+        them is greater than res_dist_cutoff. Default is 11.
 
     Returns:
-
-    - A list of Domain objects, each representing a domain with a unique
-      identifier and the range of residues it encompasses.
+      - A list of Domain objects, each representing a domain with a unique
+        identifier and the range of residues it encompasses.
 
     The logic used to determine domain membership is:
-
-    - Two residues are considered to be in the same domain if the distance
-      between them is greater than `res_dist_cutoff` and the lesser of the two
-      PAE values between them (ie min(pae[res1, res2] and pae[res2, res1])) is
-      less than `further_pae_val`, or if the distance between them is less than
-      or equal to `res_dist_cutoff` and the lesser of the two PAE values between
-      them is below `close_pae_val`. A higher PAE threshold is set for closer
-      residues, as these will always have a higher background level of confidence
-      about their relative positions.
-
-    - The function does not evaluate PAE for residue pairs less than 4 residues
-      apart due to inherently high confidence in their relative positions.
-
-    - Domains are updated or created based on the membership of the residues
-      being evaluated. If one residue is already in a domain and the other is
-      not, the latter is added to the former's domain. New domains are created
-      for pairs where neither residue is currently in a domain.
-
-    - The inner loop breaks early once a residue pair is processed and
-      determined to be in the same domain, moving to the next residue (as all
-      residues between these are assumed to be in the same domain). No gaps are
-      possible within domains
+      - Two residues are considered to be in the same domain if the distance
+        between them is greater than `res_dist_cutoff` and the lesser of the two
+        PAE values between them (ie min(pae[res1, res2] and pae[res2, res1])) is
+        less than `further_pae_val`, or if the distance between them is less than
+        or equal to `res_dist_cutoff` and the lesser of the two PAE values between
+        them is below `close_pae_val`. A higher PAE threshold is set for closer
+        residues, as these will always have a higher background level of confidence
+        about their relative positions.
+      - The function does not evaluate PAE for residue pairs less than 4 residues
+        apart due to inherently high confidence in their relative positions.
+      - Domains are updated or created based on the membership of the residues
+        being evaluated. If one residue is already in a domain and the other is
+        not, the latter is added to the former's domain. New domains are created
+        for pairs where neither residue is currently in a domain.
+      - The inner loop breaks early once a residue pair is processed and
+        determined to be in the same domain, moving to the next residue (as all
+        residues between these are assumed to be in the same domain). No gaps are
+        possible within domains
     """
     domains = []
     next_domain_num = 1
