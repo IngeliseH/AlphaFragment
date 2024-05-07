98d7933318855336c484fc70ad5ca39b6688c31a
Ingelise Holland-Kaye <160491521+IngeliseH@users.noreply.github.com>
2024-05-07 17:41:30 +0100

    removed reference to statis from conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,8 +6,7 @@
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
+# adding directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
@@ -46,8 +45,3 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
